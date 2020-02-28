19016381d00bf61323a5fe2b4a0c94080d060183
eljores <59867840+eljores@users.noreply.github.com>
2020-02-28 09:39:10 +0100

    hola

---
+++ examen_Garriga/src/examen_Garriga/examen_Garriga.java
@@ -0,0 +1,24 @@
+package examen_Garriga;
+
+public class examen_Garriga {
+
+	public static void main(String[] args) {
+		printOne();
+		printOne();
+		printTwo();
+
+	}
+
+	private static void printOne() {
+		System.out.println("Hello World");
+		
+	}
+	private static void printTwo() {
+		printOne();
+		printOne();
+		
+	}
+
+
+
+}
